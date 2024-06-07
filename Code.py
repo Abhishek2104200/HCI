e05e72443448278e31a080c589e365160be49de4
Abhishek S B <155760982+Abhishek2104200@users.noreply.github.com>
2024-06-07 21:42:20 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -49,3 +49,21 @@ print("Login successful")
  else:
 print("Invalid Login Credentials")
 
+
+#MODULE 3-Image Module (image.py):
+#Handles image loading and display
+# image.py
+from PIL import Image, ImageTk
+
+def load_image(file_path):
+ try:
+ image = Image.open(file_path)
+ return image
+ except Exception as e:
+print("Error loading image:", e)
+ return None
+
+def create_photo_image(image):
+ photo_image = ImageTk.PhotoImage(image)
+ return photo_image
+
